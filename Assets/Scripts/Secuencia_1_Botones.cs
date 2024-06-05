97105260b0a9bb253c1a57d875ed420fad5a27de
Ximena Hidalgo <122484522+XimenaFHH@users.noreply.github.com>
2024-06-05 14:54:29 -0600

    Scripts
    Pruebas de Scripts

--- Assets/Scripts/Secuencia_1_Botones.cs
+++ Assets/Scripts/Secuencia_1_Botones.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class Secuencia_1_Botones : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Button Sosa;
+    public Button Añadir;
+
+    public GameObject EnvaseSosa; // Asigna el GameObject del modelo aquí
+    public GameObject CucharaSosa; // Asigna el GameObject del modelo aquí
+    private Animator animator; // Referencia al Animator del modelo
+
+
+
+    private Button firstPressedButton;//Almacena el botón presionado primero
+    private bool isSecondButtonPending = false;//Bandera para indicar si se está esperando el segundo botón
+
+
+    private void Start()
     {
-        
+        animator = EnvaseSosa.GetComponent<Animator>(); // Obtiene el Animator del modelo
+        animator = CucharaSosa.GetComponent<Animator>(); // Obtiene el Animator del modelo
+
+
+    }
+    
+    public void OnButton1Down()
+    {
+        firstPressedButton = Sosa;
+        isSecondButtonPending = true;
+        animator.Play("Envase Rotación"); // Reproduce la animación predeterminada
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnButton2Down()
     {
-        
+        if (isSecondButtonPending)
+        {
+            ActivateModel(); // Activa el modelo antes de la animación
+            animator.Play("CucharaSosa"); // Reproduce la animación predeterminada
+            isSecondButtonPending = false;
+        }
     }
+
+    public void ActivateModel()
+
+    {
+        CucharaSosa.SetActive(true);
+    }
+
+    public void DeactivateModel()
+    {
+        CucharaSosa.SetActive(false);
+    }
+
 }
