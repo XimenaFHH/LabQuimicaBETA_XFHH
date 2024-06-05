cbb53c0ac44c5e2fbb189b9f2f3a0b08e2ed0508
Ximena Hidalgo <122484522+XimenaFHH@users.noreply.github.com>
2024-06-05 13:55:07 -0600

    Organizando

---
+++ Assets/Scripts/Secuencia_1_Botones.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Secuencia_1_Botones : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
